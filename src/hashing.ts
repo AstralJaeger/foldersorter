1cc9622bcc7085f9243435904df3b972ea18613a
Felix Hillebrand <97hilfel@gmail.com>
2022-05-22 09:17:36 +0200

    Implement stream based hashing for big files

---
+++ src/hashing.ts
@@ -0,0 +1,36 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+
+export async function hashFile(
+    filePath: string,
+    algorithm: string = 'sha256',
+    encoding: string = 'hex'
+): Promise<string> {
+    const hash = crypto.createHash(algorithm);
+
+    hash.setEncoding(
+        <
+            | 'ascii'
+            | 'utf8'
+            | 'utf-8'
+            | 'utf16le'
+            | 'ucs2'
+            | 'ucs-2'
+            | 'base64'
+            | 'base64url'
+            | 'latin1'
+            | 'binary'
+            | 'hex'
+        >encoding
+    );
+
+    return new Promise((resolve, reject) => {
+        const input = fs.createReadStream(filePath);
+
+        input.on('end', () => {
+            hash.end();
+            resolve(hash.read());
+        });
+        input.pipe(hash);
+    });
+}
